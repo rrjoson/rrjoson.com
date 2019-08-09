ba0a19a6e37d7f634559542d6fe6c11075e10f3f
rrjoson <rrjoson08@gmail.com>
2019-08-09 10:00:15 +0800

    Update about me

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import rr from '../../content/images/rr.jpg'
+
 
 export default class UserInfo extends Component {
   render() {
@@ -6,8 +8,16 @@ export default class UserInfo extends Component {
       <aside className="note">
         <div className="container note-container">
           <div className="flex-author">
+            <div className="flex-avatar">
+              <img className="avatar" src={rr} alt="RR Joson" />
+            </div>
             <div>
-              <p>This is a stub</p>
+              <p>
+                {`I'm RR, a classically trained pianist turned
+                software engineer who loves working with UX designers
+                to build interfaces that are a delight to use.
+                `}
+              </p>
             </div>
           </div>
         </div>
