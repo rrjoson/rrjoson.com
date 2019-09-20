83812dc1007c2e27428ef1f2d67b2d36818a28cf
rrjoson <rrjoson08@gmail.com>
2019-09-20 12:16:46 +0800

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,20 @@ export default class Footer extends Component {
     return (
       <footer className="footer container">
         <a
+          href="https://dev.to/rrjoson"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dev.to
+        </a>
+         <a
+          href="https://www.linkedin.com/in/rrjoson"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
           href="https://twitter.com/rrjoson"
           target="_blank"
           rel="noopener noreferrer"
